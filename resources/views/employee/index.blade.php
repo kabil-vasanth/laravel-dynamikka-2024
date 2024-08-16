248d562c79cc63ce826cf270d57c7304bf1dc9c5
kabil-vasanth <89719783+kabil-vasanth@users.noreply.github.com>
2024-08-16 11:58:47 +0530

    update

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -24,21 +24,21 @@
 
 
         </tr>
-        @foreach ($employees as $employeess)
+        @foreach ($employees as $employee)
 
 
 
         <tr>
 
 
-            <td>{{$employeess->firstname}}</td>
-            <td>{{$employeess->lastname}}</td>
-            <td>{{$employeess->date_of_birth}}</td>
-            <td>{{$employeess->nic_number}}</td>
-            <td>{{$employeess->join_date}}</td>
-            <td>{{$employeess->gender}}</td>
-            <td>{{$employeess->district}}</td>
-            <td><a href="{{url("employee/$employeess->id")}}">show</a></td>
+            <td>{{$employee->firstname}}</td>
+            <td>{{$employee->lastname}}</td>
+            <td>{{$employee->date_of_birth}}</td>
+            <td>{{$employee->nic_number}}</td>
+            <td>{{$employee->join_date}}</td>
+            <td>{{$employee->gender}}</td>
+            <td>{{$employee->district}}</td>
+            <td><a href="{{url("employee/$employee->id")}}">show</a></td>
 
         </tr>
 
