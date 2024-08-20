07f0f662e71655723b9e4c758c2410824e6d61a6
kabil-vasanth <89719783+kabil-vasanth@users.noreply.github.com>
2024-08-20 20:04:29 +0530

    update

--- database/migrations/2024_08_15_110038_create_students_table.php
+++ database/migrations/2024_08_15_110038_create_students_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('firstname', length: 50);
             $table->string('lastname', length: 50);
+            $table->integer('grade_id');
+
             $table->timestamps();
         });
     }
