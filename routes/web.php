60c22875874cf7954e3b13545a9a651df838535e
kabil-vasanth <89719783+kabil-vasanth@users.noreply.github.com>
2024-08-24 10:20:47 +0530

    da

--- routes/web.php
+++ routes/web.php
@@ -4,51 +4,56 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Student;
 use App\Models\Grade;
 use App\Models\Subject;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\SubjectController;
 
-Route::get('/students', function () {
+// Route::get('/students', [StudentController::class, 'index']);
+// Route::get('/students/{id}', [StudentController::class, 'show']);
 
-     $student=Student::all();
-    // $student = DB::Select(*from students')->get();
+// ----------------method 1----------
+// Route::resource('/students', StudentController::class);
+// Route::resource('/subjects', SubjectController::class);
+// Route::resource('/grades', GradeController::class);
+//---------method 2-----------
+Route::resources(([
+    'students'=>StudentController::class,
+    'grades'=>GradeController::class,
+    'subjects'=>SubjectController::class,
+]));
 
+// Route::get('/students/{id}', function ($id) {
 
-
-
-    return view('student.index',compact('student'));
-
-});
-
-Route::get('/students/{id}', function ($id) {
-
-    $student=Student::find($id);
-
-
-
-
-    return view('grade.show',compact('student'));
-
-});
+//     $student=Student::find($id);
 
 
 
-Route::get('/grades/{id}', function ($id) {
 
-    $grade=Grade::find($id);
-    $students=Grade::find($id)->students;
+//     return view('grade.show',compact('student'));
 
-
-
-    return view('grade.index',compact('grade','students'));
-
-});
-
-Route::get('/subjects/{id}', function ($id) {
-
-    $subjects=Subject::find($id);
-    // $students=Grade::find($id)->students;
+// });
 
 
 
-    return view('subject.index',compact('subjects'));
+// Route::get('/grades/{id}', function ($id) {
 
-});
+//     $grade=Grade::find($id);
+//     $students=Grade::find($id)->students;
+
+
+
+//     return view('grade.index',compact('grade','students'));
+
+// });
+
+// Route::get('/subjects/{id}', function ($id) {
+
+//     $subjects=Subject::find($id);
+//     // $students=Grade::find($id)->students;
+
+
+
+//     return view('subject.index',compact('subjects'));
+
+// });
 
